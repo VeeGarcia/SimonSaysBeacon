02030f7d1c933944a9a35ef8fadbd3d025ad7e98
Veronica Garcia <v.garcia@maple-apps.com>
2014-04-03 19:01:06 +0200

    init

---
+++ SimonSaysBeacon/SimonSaysBeacon/SSBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSBAppDelegate.h
+//  SimonSaysBeacon
+//
+//  Created by Veronica Garcia on 03.04.14.
+//  Copyright (c) 2014 Veronica Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimonSaysBeacon/SimonSaysBeacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimonSaysBeacon
+//
+//  Created by Veronica Garcia on 03.04.14.
+//  Copyright (c) 2014 Veronica Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSBAppDelegate class]));
+    }
+}
