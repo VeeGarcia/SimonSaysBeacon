7ac7884b1aefba1e5b584a3ae44becec73ad074e
Christoph Jerolimov <jerolimov+git+2014@gmail.com>
2014-04-03 19:21:42 +0200

    Add Storyboard and UIViewController.

---
+++ SimonSaysBeacon/SimonSaysBeacon/SSBGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSBGameViewController.h
+//  SimonSaysBeacon
+//
+//  Created by Christoph Jerolimov on 03.04.14.
+//  Copyright (c) 2014 Veronica Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSBGameViewController : UIViewController
+
+@end
